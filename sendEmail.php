d331d1a7e63a8a5e770b0b44fccba7b7a923fcef
Dejan Petrović <dekip@beotel.rs>
2018-01-01 12:29:01 +0100

    Update sendEmail.php

--- sendEmail.php
+++ sendEmail.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * Skeč za potrebe članka u Svetu Kompjutera
+ * Author: Petrović Dejan
+ * Date: 31/12/2017
+ * Wemos D1, LM35
+ */
 $lm35 = $_GET["lm35"];
 $text ="Temperatura: ".$lm35." C";
 $email 	= "dekip@beotel.rs";
